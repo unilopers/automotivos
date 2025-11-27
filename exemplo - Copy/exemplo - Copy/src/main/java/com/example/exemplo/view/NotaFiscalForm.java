3f39db55f95249944a1ed2de5977a630f2b1ee20
JPmangerona <joaopedromangerona@edu.unifil.br>
2025-11-26 21:58:55 -0300

    ✨ adicionado nota fiscal

---
+++ exemplo - Copy/exemplo - Copy/src/main/java/com/example/exemplo/view/NotaFiscalForm.java
@@ -0,0 +1,13 @@
+package com.example.exemplo.view;
+
+public class NotaFiscalForm {
+    private Integer idPedido;
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+}
