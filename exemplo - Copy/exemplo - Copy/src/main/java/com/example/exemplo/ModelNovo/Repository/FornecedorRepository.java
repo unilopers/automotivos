3b41d90319fa4d65ec348213c2c4b5f5df1b4e84
rafaeljoaoc <rafaeljoaoc@edu.unifil.br>
2025-11-26 21:48:08 -0300

    ✨Adição de classes ao Repository

---
+++ exemplo - Copy/exemplo - Copy/src/main/java/com/example/exemplo/ModelNovo/Repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.example.exemplo.ModelNovo.Repository;
+
+import com.example.exemplo.ModelNovo.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
+}
